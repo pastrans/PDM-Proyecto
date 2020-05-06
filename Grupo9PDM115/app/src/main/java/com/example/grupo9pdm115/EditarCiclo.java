b8c80fd34107914f6676f7ef8842950a226c5a16
Vicnava95 <47409039+Vicnava95@users.noreply.github.com>
2020-05-06 16:50:03 -0600

    Gestionar Ciclo
    Interfaces para CRUD del ciclo

--- Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/EditarCiclo.java
+++ Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/EditarCiclo.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class EditarCiclo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        //setContentView(R.layout.activity_gestionar_ciclo);
-        //setContentView(R.layout.activity_nuevo_ciclo);
         setContentView(R.layout.activity_editar_ciclo);
     }
 }
---
+++ Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/GestionarCiclo.java
@@ -0,0 +1,20 @@
+package com.example.grupo9pdm115;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class GestionarCiclo extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestionar_ciclo);
+    }
+    }
+//Falta agregar la lista para mostrar los datos
+// Link para tablas dinámicas
+//https://programaressencillo.wordpress.com/2014/11/22/android-tablas-dinamicas-en-android/
+// Link para ListView
+//http://www.hermosaprogramacion.com/2014/10/android-listas-adaptadores/

77379dab0fe5324b03f4cdf26fe53cec05eadb59
caceres08 <carlos_08-caceres@hotmail.com>
2020-05-06 20:31:59 -0600

    Gestionar Feriado
    Pantallas de Gestionar Feriado

--- Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/MainActivity.java
+++ Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/MainActivity.java
@@ -2,9 +2,19 @@ package com.example.grupo9pdm115;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    //Variables
+    TextView tv;
+    Calendar mCurrentDate;
+    int day, month, year;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +22,32 @@ public class MainActivity extends AppCompatActivity {
         //setContentView(R.layout.activity_main);
         //setContentView(R.layout.activity_gestionar_ciclo);
         //setContentView(R.layout.activity_nuevo_ciclo);
-        setContentView(R.layout.activity_editar_ciclo);
+        //setContentView(R.layout.activity_editar_ciclo);
+        setContentView(R.layout.activity_nuevo_feriado);
+        tv=(TextView) findViewById(R.id.fechaInicio);
+
+        mCurrentDate= Calendar.getInstance();
+
+        day= mCurrentDate.get(Calendar.DAY_OF_MONTH);
+        month= mCurrentDate.get(Calendar.MONTH);
+        year= mCurrentDate.get(Calendar.YEAR);
+
+        month= month+1;
+        tv.setText(day+"/"+month+"/"+year);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        monthOfYear= monthOfYear+1;
+                        tv.setText(dayOfMonth+"/"+monthOfYear+"/"+year);
+                    }
+                },year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
     }
 }

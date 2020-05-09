0c30f8817bfa480dc6c96abcb694b4df84300841
pastrans <pastran2526@gmail.com>
2020-05-08 18:56:00 -0600

    Modelos de: Grupo,TipoGrupo,Unidad

--- Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/Modelos/Grupo.java
+++ Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/Modelos/Grupo.java
@@ -1,4 +1,34 @@
 package com.example.grupo9pdm115.Modelos;
 
 public class Grupo {
+    private int idGrupo;
+    private int numero;
+
+    // -------------Constructor---------------
+    public Grupo(int idGrupo, int numero) {
+        this.idGrupo = idGrupo;
+        this.numero = numero;
+    }
+    // -------------Fin del Constructor---------------
+    // -------------Métodos getter y setter---------------
+    public int getIdGrupo() {
+        return idGrupo;
+    }
+
+    public void setIdGrupo(int idGrupo) {
+        this.idGrupo = idGrupo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+// -------------Fin de Métodos getter y setter---------------
+
+
+
 }
+

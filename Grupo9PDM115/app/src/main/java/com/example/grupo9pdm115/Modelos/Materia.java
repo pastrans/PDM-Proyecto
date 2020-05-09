f03dac78f822e19e5225706d559e68eafda53c3e
Pablo Avelar <am16015@ues.edu.sv>
2020-05-08 21:43:25 -0600

    Agregados atributos y métodos para función de la BD, en particular inserción

--- Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/Modelos/Materia.java
+++ Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/Modelos/Materia.java
@@ -1,6 +1,15 @@
 package com.example.grupo9pdm115.Modelos;
 
+import android.content.ContentValues;
+
+// Importación de ControlBD
+import com.example.grupo9pdm115.BD.ControlBD;
+
 public class Materia {
+    // Atributos para BD
+    private final String nombreTabla = "materia";
+    private ContentValues valores = new ContentValues();
+
     // Atributos
     private String codMateria;
     private int idUnidad;
@@ -10,7 +19,7 @@ public class Materia {
     // Métodos
     public Materia(){
     }
-    
+
     public Materia(String codMateria, int idUnidad, String nombreMateria, boolean masiva) {
         this.codMateria = codMateria;
         this.idUnidad = idUnidad;
@@ -18,35 +27,32 @@ public class Materia {
         this.masiva = masiva;
     }
 
-    public String getCodMateria() {
-        return codMateria;
-    }
+    public String getCodMateria() { return codMateria; }
 
-    public void setCodMateria(String codMateria) {
-        this.codMateria = codMateria;
-    }
+    public void setCodMateria(String codMateria) { this.codMateria = codMateria; }
 
-    public int getIdUnidad() {
-        return idUnidad;
-    }
+    public int getIdUnidad() { return idUnidad; }
 
-    public void setIdUnidad(int idUnidad) {
-        this.idUnidad = idUnidad;
-    }
+    public void setIdUnidad(int idUnidad) { this.idUnidad = idUnidad; }
 
-    public String getNombreMateria() {
-        return nombreMateria;
-    }
+    public String getNombreMateria() { return nombreMateria; }
 
-    public void setNombreMateria(String nombreMateria) {
-        this.nombreMateria = nombreMateria;
-    }
+    public void setNombreMateria(String nombreMateria) { this.nombreMateria = nombreMateria; }
 
-    public boolean isMasiva() {
-        return masiva;
-    }
+    public boolean isMasiva() { return masiva; }
 
-    public void setMasiva(boolean masiva) {
-        this.masiva = masiva;
+    public void setMasiva(boolean masiva) { this.masiva = masiva; }
+
+    // Métodos para BD
+    public String getNombreTabla() { return nombreTabla; }
+
+    public ContentValues getValores(){
+        // Agregando los valores de los atributos al content value
+        valores.put("codMateria", getCodMateria());
+        valores.put("idUnidad", getIdUnidad());
+        valores.put("nombreMateria", getNombreMateria());
+        valores.put("masiva", isMasiva());
+
+        return valores;
     }
 }

2a7e070bab9505c42a618e69f096841a4bc1dca3
Oscar Lizama <oslizama@gmail.com>
2020-06-04 13:33:30 -0600

    Clase de Sesion creada

--- Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/iniciarSesion.java
+++ Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/iniciarSesion.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class iniciarSesion extends AppCompatActivity {
+public class IniciarSesion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

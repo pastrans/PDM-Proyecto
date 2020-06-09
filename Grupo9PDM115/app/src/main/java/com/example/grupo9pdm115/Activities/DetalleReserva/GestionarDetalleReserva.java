9d4968e2d01f1ede4fbd02c4606120b679557341
caceres08 <carlos_08-caceres@hotmail.com>
2020-06-09 16:58:24 -0600

    DetalleReserva
    Se agregaron algunos campos

---
+++ Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/Activities/DetalleReserva/GestionarDetalleReserva.java
@@ -0,0 +1,15 @@
+package com.example.grupo9pdm115.Activities.DetalleReserva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.grupo9pdm115.R;
+
+public class GestionarDetalleReserva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestionar_detalle_reserva);
+    }
+}

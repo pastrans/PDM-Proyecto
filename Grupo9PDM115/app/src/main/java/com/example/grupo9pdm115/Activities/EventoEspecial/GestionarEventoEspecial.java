8618d7debe8ef1a03a70a6ab83956f63879a86db
Vicnava95 <47409039+Vicnava95@users.noreply.github.com>
2020-06-10 01:03:27 -0600

    Activitys principales
    Activitys de Evento,Solicitud, Detalle Reserva

--- Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/Activities/EventoEspecial/GestionarEventoEspecial.java
+++ Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/Activities/EventoEspecial/GestionarEventoEspecial.java
@@ -2,7 +2,10 @@ package com.example.grupo9pdm115.Activities.EventoEspecial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import com.example.grupo9pdm115.R;
 
 public class GestionarEventoEspecial extends AppCompatActivity {
@@ -11,5 +14,9 @@ public class GestionarEventoEspecial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestionar_evento_especial);
+    }
+    public void btnNuevoEventoEspecial(View v){
+        Intent inte = new Intent(this, NuevoEventoEspecial.class);
+        startActivity(inte);
     }
 }

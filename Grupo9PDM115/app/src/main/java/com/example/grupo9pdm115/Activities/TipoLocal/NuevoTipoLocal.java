14807c8148aaff1ec5da53c858b7df25ec80cbf1
Pablo Avelar <am16015@ues.edu.sv>
2020-06-18 16:00:32 -0600

    Val usuario y sesión Editar/Nuevo TipoLocal

--- Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/Activities/TipoLocal/NuevoTipoLocal.java
+++ Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/Activities/TipoLocal/NuevoTipoLocal.java
@@ -2,6 +2,7 @@ package com.example.grupo9pdm115.Activities.TipoLocal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,9 +11,11 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.grupo9pdm115.Activities.ErrorDeUsuario;
 import com.example.grupo9pdm115.Adapters.UsuarioAdapter;
 import com.example.grupo9pdm115.BD.ControlBD;
 import com.example.grupo9pdm115.Modelos.Encargado;
+import com.example.grupo9pdm115.Modelos.Sesion;
 import com.example.grupo9pdm115.Modelos.TipoLocal;
 import com.example.grupo9pdm115.R;
 import com.example.grupo9pdm115.Spinners.UsuarioSpinner;
@@ -31,6 +34,16 @@ public class NuevoTipoLocal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Validando usuario y sesión
+        if((Sesion.getLoggedIn(getApplicationContext()) && !Sesion.getAccesoUsuario(getApplicationContext(), "ITL"))
+                || !Sesion.getLoggedIn(getApplicationContext())){
+            Intent intent = new Intent(this, ErrorDeUsuario.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            // Estas banderas borran la tarea actual y crean una nueva con la actividad iniciada
+            startActivity(intent);
+            finish();
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_tipo_local);
         helper = new ControlBD(this);

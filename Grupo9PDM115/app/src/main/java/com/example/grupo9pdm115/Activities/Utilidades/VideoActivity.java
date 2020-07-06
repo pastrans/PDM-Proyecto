3e600e807bbe7c64bca373704844b305971ce835
Vicnava95 <47409039+Vicnava95@users.noreply.github.com>
2020-07-05 18:00:45 -0600

    activity_video

---
+++ Grupo9PDM115/app/src/main/java/com/example/grupo9pdm115/Activities/Utilidades/VideoActivity.java
@@ -0,0 +1,16 @@
+package com.example.grupo9pdm115.Activities.Utilidades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.grupo9pdm115.R;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+    }
+}
